5566b86b8f40eab34992b6bc29d33751ff3ab135
Derek Menéndez <derekurizar@gmail.com>
2018-04-21 18:50:16 -0600

    Se contagian los cuadritos :D
    no pos se contagian?

---
+++ ProyectoPoblacion/ProyectoPoblacion/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ProyectoPoblacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPoblacion = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCiclo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPoblacion)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvPoblacion
+            // 
+            this.dgvPoblacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPoblacion.ColumnHeadersVisible = false;
+            this.dgvPoblacion.Location = new System.Drawing.Point(12, 12);
+            this.dgvPoblacion.Name = "dgvPoblacion";
+            this.dgvPoblacion.RowHeadersVisible = false;
+            this.dgvPoblacion.Size = new System.Drawing.Size(403, 403);
+            this.dgvPoblacion.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCiclo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 439);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(952, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opciones";
+            // 
+            // btnCiclo
+            // 
+            this.btnCiclo.Location = new System.Drawing.Point(29, 47);
+            this.btnCiclo.Name = "btnCiclo";
+            this.btnCiclo.Size = new System.Drawing.Size(101, 23);
+            this.btnCiclo.TabIndex = 0;
+            this.btnCiclo.Text = "Siguiente Ciclo";
+            this.btnCiclo.UseVisualStyleBackColor = true;
+            this.btnCiclo.Click += new System.EventHandler(this.btnCiclo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 551);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvPoblacion);
+            this.Name = "Form1";
+            this.Text = "Población Enfermos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPoblacion)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPoblacion;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCiclo;
+    }
+}
+
---
+++ ProyectoPoblacion/ProyectoPoblacion/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPoblacion
+{
+    public partial class Form1 : Form
+    {
+        private const int pop = 400;
+        private List<int> enfermos;
+        private Random rnd;
+        private int counter;
+        private int n;
+        private int k;
+        private bool turn;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Inicialización del grid
+            InitializeGrid();
+
+            //Inicialización de enfermo
+            rnd = new Random();
+            n = rnd.Next(1, 400);
+            dgvPoblacion.Rows[GetY(n)].Cells[GetX(n)].Style.BackColor = Color.Green;
+            enfermos = new List<int>();
+            enfermos.Add(n);
+            k = 2;
+            turn = true;
+        }
+
+        /// <summary>
+        /// Metodo que inicializa el grid
+        /// </summary>
+        private void InitializeGrid()
+        {
+            dgvPoblacion.RowCount = 20;
+            dgvPoblacion.ColumnCount = 20;
+
+            for (int i = 0; i < 20; i++)
+            {
+                dgvPoblacion.Rows[i].Height = 20;
+                dgvPoblacion.Columns[i].Width = 20;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene la posicion Y de un grid 20*20 sobre un numero entre 1-400
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        private int GetY(int pos)
+        {
+            return pos % 20 == 0 ? pos / 20 - 1 : pos / 20;
+        }
+
+        /// <summary>
+        /// Obtiene la posicion X de un grid 20*20 sobre un numero entre 1-400
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        private int GetX(int pos)
+        {
+            return pos % 20 == 0 ? 19 : pos % 20 - 1;
+        }
+
+        /// <summary>
+        /// Metodo que contagia cuadritos.
+        /// -21 -20 -19
+        ///  -1  C  +1
+        /// +19 +20 +21
+        /// </summary>
+        private void Contagiar()
+        {
+            n = enfermos.Count;
+            //Contagio
+            for (int i=0;i < n;i++)
+            {
+                counter = 0;
+                while (counter != k)
+                {
+                    try
+                    {
+                        switch (rnd.Next(1, 8))
+                        {
+                            case 1:
+                                dgvPoblacion.Rows[GetY(enfermos[i] - 21)].Cells[GetX(enfermos[i] - 21)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] - 21);
+                                counter++;
+                                break;
+                            case 2:
+                                dgvPoblacion.Rows[GetY(enfermos[i] - 20)].Cells[GetX(enfermos[i] - 20)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] - 20);
+                                counter++;
+                                break;
+                            case 3:
+                                dgvPoblacion.Rows[GetY(enfermos[i] - 19)].Cells[GetX(enfermos[i] - 19)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] - 19);
+                                counter++;
+                                break;
+                            case 4:
+                                dgvPoblacion.Rows[GetY(enfermos[i] - 1)].Cells[GetX(enfermos[i] - 1)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] - 1);
+                                counter++;
+                                break;
+                            case 5:
+                                dgvPoblacion.Rows[GetY(enfermos[i] + 1)].Cells[GetX(enfermos[i] + 1)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] + 1);
+                                counter++;
+                                break;
+                            case 6:
+                                dgvPoblacion.Rows[GetY(enfermos[i] + 19)].Cells[GetX(enfermos[i] + 19)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] + 19);
+                                counter++;
+                                break;
+                            case 7:
+                                dgvPoblacion.Rows[GetY(enfermos[i] + 20)].Cells[GetX(enfermos[i] + 20)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] + 20);
+                                counter++;
+                                break;
+                            case 8:
+                                dgvPoblacion.Rows[GetY(enfermos[i] + 21)].Cells[GetX(enfermos[i] + 21)].Style.BackColor = Color.Green;
+                                VerificarContagio(enfermos[i] + 21);
+                                counter++;
+                                break;
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException a)
+                    {
+                        counter++;
+                    }
+                }
+            }          
+        }
+
+
+        /// <summary>
+        /// Verifica el contagio y lo agrega a la lista
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="symbol"></param>
+        /// <param name="mov"></param>
+        private void VerificarContagio(int c)
+        {
+            if (c <= 0 || c >= 401) return;
+            if (!enfermos.Contains(c))
+            {
+                enfermos.Add(c);
+            }
+        }
+
+        /// <summary>
+        /// Metodo que moviliza a los enfermos.
+        /// </summary>
+        private void Movilizar()
+        {
+            if (enfermos.Count == 400)
+            {
+                return;
+            }
+
+            for (int i = 0; i < enfermos.Count; i++)
+            {
+                dgvPoblacion.Rows[GetY(enfermos[i])].Cells[GetX(enfermos[i])].Style.BackColor = Color.White;
+                n = rnd.Next(1, 400);
+
+                if (dgvPoblacion.Rows[GetY(n)].Cells[GetX(n)].Style.BackColor == Color.Green)
+                {
+                    i--;                   
+                }
+                else
+                {
+                    enfermos[i] = n;
+                    dgvPoblacion.Rows[GetY(n)].Cells[GetX(n)].Style.BackColor = Color.Green;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Limpia la seleccion del data grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvPoblacion.ClearSelection();
+        }
+
+        /// <summary>
+        /// Cada dos clicks es un ciclo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCiclo_Click(object sender, EventArgs e)
+        {
+            if (turn)
+            {
+                Movilizar();
+                turn = false;
+            }
+            else
+            {
+                Contagiar();
+                turn = true;
+            }
+        }
+    }   
+}
